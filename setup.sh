37f9a60f934118d0bddbcebb8b0055c9a19e1b1e
retwere <retwere@gmail.com>
2016-09-09 16:28:13 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -233,7 +233,7 @@ $CURL -X POST -d @- -H "Content-type: application/json" \
       "root": "$ROOT_DIR",
       "port": $PORT,
       "content-database": "$DB",
-      "modules-database": 0,
+      "modules-database": "file-system",
       "threads": 64,
       "default-user": "$USER",
       "authentication": "application-level"
